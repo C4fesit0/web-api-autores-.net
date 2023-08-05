b6d924f8bf48031a40f4271d97ca464481f1ad29
Alan Lazo <al4nlaz0@gmail.com>
2023-08-05 00:52:34 -0300

    autor model and controller added

---
+++ WebApiAutores/AutoresController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entities;
+
+namespace WebApiAutores
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet] public ActionResult<List<Autor>> Index()
+        {
+            return new List<Autor>(){ 
+                new Autor() { Id = 1, Name = "Nahue" },
+                new Autor() { Id = 2, Name = "Franco" },
+            };
+        }
+    }
+}

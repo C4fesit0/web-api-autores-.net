18949eda2e9f2feb92c3d508b0abb7bc906341e3
Alan Lazo <al4nlaz0@gmail.com>
2023-08-05 11:42:03 -0300

    added put and delete method to AutoresController

--- WebApiAutores/AutoresController.cs
+++ WebApiAutores/AutoresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApiAutores.Entities;
 
 namespace WebApiAutores
@@ -7,12 +8,58 @@ namespace WebApiAutores
     [Route("api/autores")]
     public class AutoresController: ControllerBase
     {
-        [HttpGet] public ActionResult<List<Autor>> Index()
-        {
-            return new List<Autor>(){ 
-                new Autor() { Id = 1, Name = "Nahue" },
-                new Autor() { Id = 2, Name = "Franco" },
-            };
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context) {
+            this.context = context;
         }
+
+
+
+        [HttpGet] public async Task<ActionResult<List<Autor>>> Index()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost] public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok(autor);
+        }
+
+        [HttpPut("{id:int}")] public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el Id de la url");
+            }
+
+            var exists = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!exists)
+            {
+                return NotFound("Autor no encontrado");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok(autor);
+        }
+
+        [HttpDelete("{id:int}")] public async Task<ActionResult> Delete(int id)
+        {
+            var exists = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if(!exists)
+            {
+                return NotFound("Autor no encontrado");
+            }
+
+            context.Remove(new Autor() { Id= id});
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }

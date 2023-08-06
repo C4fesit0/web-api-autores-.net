bf9820478da33b59d1084fade81dcf7a900ba452
Alan Lazo <al4nlaz0@gmail.com>
2023-08-06 14:11:34 -0300

    added Libros

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -2,35 +2,39 @@
 using Microsoft.EntityFrameworkCore;
 using WebApiAutores.Entities;
 
-namespace WebApiAutores
+namespace WebApiAutores.Controllers
 {
     [ApiController]
     [Route("api/autores")]
-    public class AutoresController: ControllerBase
+    public class AutoresController : ControllerBase
     {
         private readonly ApplicationDbContext context;
 
-        public AutoresController(ApplicationDbContext context) {
+        public AutoresController(ApplicationDbContext context)
+        {
             this.context = context;
         }
 
 
 
-        [HttpGet] public async Task<ActionResult<List<Autor>>> Index()
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Index()
         {
-            return await context.Autores.ToListAsync();
+            return await context.Autores.Include(x=> x.Libros).ToListAsync();
         }
 
-        [HttpPost] public async Task<ActionResult> Post(Autor autor)
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
         {
             context.Add(autor);
             await context.SaveChangesAsync();
             return Ok(autor);
         }
 
-        [HttpPut("{id:int}")] public async Task<ActionResult> Put(Autor autor, int id)
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Autor autor, int id)
         {
-            if(autor.Id != id)
+            if (autor.Id != id)
             {
                 return BadRequest("El id del autor no coincide con el Id de la url");
             }
@@ -47,16 +51,17 @@ namespace WebApiAutores
             return Ok(autor);
         }
 
-        [HttpDelete("{id:int}")] public async Task<ActionResult> Delete(int id)
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
         {
             var exists = await context.Autores.AnyAsync(x => x.Id == id);
 
-            if(!exists)
+            if (!exists)
             {
                 return NotFound("Autor no encontrado");
             }
 
-            context.Remove(new Autor() { Id= id});
+            context.Remove(new Autor() { Id = id });
             await context.SaveChangesAsync();
             return Ok();
         }
